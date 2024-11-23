d1702466167402c5ce806fde92787abdbbdab2c3
Unggar Madi <39094754+unggarmadi21@users.noreply.github.com>
2024-11-23 22:11:37 +0700

    Update file index.php

--- index.php
+++ index.php
@@ -16,6 +16,7 @@
 
 <head>
     <meta charset="UTF-8">
+    <meta name="dicoding:email" content="unggarmadi@gmail.com">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="shortcut icon" href="img/favicon.png">
